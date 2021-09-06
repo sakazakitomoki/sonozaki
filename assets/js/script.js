720f9c2b9c31e89ee46a7ef264501c8b0374ddd2
sakazakitomoki <st04020508@gmail.com>
2021-09-06 23:38:54 +0900

    トップページ完成

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,45 @@
 "use strict";
 
+var delay = function delay() {
+  var spinner = document.getElementById('js-loading');
+  spinner.classList.add('is-loaded');
+};
+
+window.onload = setTimeout(delay, 2000);
 jQuery(function ($) {
-  $('p').toggleClass('is-active');
+  $('.js-baricon').on('click', function () {
+    $('.c-drawer').addClass('is-active');
+  });
+  $('.js-batsu').on('click', function () {
+    $('.c-drawer').removeClass('is-active');
+  });
+  $('.c-drawer__link').click(function () {
+    $('.c-drawer').removeClass('is-active');
+  });
+  $('a[href^="#"]').click(function () {
+    // hrefが『#』で始まるリンクのものをクリックした時
+    var speed = 500; // スクロールの速さ数字が小さいほど速い
+
+    var href = $(this).attr('href'); // href属性の値を取得
+
+    var target = $(href == "#" || href == "" ? 'html' : href); // hrefが#もしくは空の時はtargetに(′html′)が代入、それ以外は(href)がtargetに代入される。
+
+    var position = target.offset().top - 56; // targetまでの位置を取得 +100はheaderの高さ
+
+    $('html, body').animate({
+      scrollTop: position
+    }, speed, 'swing'); // animateプロパティで動きを決める
+
+    return false;
+  });
+  $(window).scroll(function () {
+    var $hero = $('.js-hero').innerHeight();
+
+    if ($(this).scrollTop() > $hero) {
+      $('.js-header').addClass('is-open');
+    } else {
+      $('.js-header').removeClass('is-open');
+    }
+  });
 });
 //# sourceMappingURL=map/script.js.map
